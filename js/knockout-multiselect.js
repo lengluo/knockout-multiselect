d95da3a8871ac506fc68de443b2c3055cf5ecd18
Luís Rudge <luis@luisrudge.net>
2013-05-02 17:43:41 -0300

    added namespace to js viewmodel

--- js/knockout-multiselect.js
+++ js/knockout-multiselect.js
@@ -16,3 +16,63 @@ ko.bindingHandlers.multiselect = {
         console.log('update');
     }
 };
+
+    ko.knockoutMultiSelectViewModel = function (parameters) {
+    var self = this;
+    //knockout options
+    self.options = ko.observableArray(parameters.options || []);
+    self.selectedOptions = ko.observableArray(parameters.selectedOptions || []);
+    self.optionsText = parameters.optionsText || '';
+    self.optionsValue = parameters.optionsValue || '';
+
+    //custom options
+    self.originalOptions = ko.observableArray(parameters.options || []);
+
+
+    self.filter = ko.observable('');
+    self.enableSearch = ko.observable(parameters.enableSearch || false);
+    self.enableSelectAll = ko.observable(parameters.enableSelectAll || false);
+
+    self.selectAll = function () {
+        if (!(self.optionsValue || self.optionsText)) {
+            self.selectedOptions(self.options());
+            return;
+        }
+
+        self.selectedOptions([]);
+        $.each(self.options(), function (index, value) {
+            self.selectedOptions.push(value[self.optionsValue || self.optionsText].toString());
+        });
+    };
+
+    self.clearSelected = function () {
+        self.selectedOptions([]);
+    };
+
+    self.getValueFor = function (data) {
+        if (self.optionsValue || self.optionsText) return data[self.optionsValue || self.optionsText];
+        return data;
+    };
+
+    self.getTextFor = function (data) {
+        if (self.optionsText) return data[self.optionsText];
+        return data;
+    };
+
+    self.filter.subscribe(function (newValue) {
+
+        if (!newValue) {
+            self.options(self.originalOptions());
+            return;
+        }
+
+        self.options([]);
+        var array = self.originalOptions();
+        for (var i = 0; i < array.length; i++) {
+            var item = array[i];
+            if (self.getTextFor(item).substring(0, self.filter().length).toLowerCase() == self.filter().toLowerCase()) {
+                self.options.push(item);
+            }
+        }
+    });
+};
